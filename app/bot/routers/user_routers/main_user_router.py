1979dd0363c2e59407703bed8f5db908032079d4
viss <egor-berezin@bk.ru>
2025-06-17 10:41:54 +1000

    commit

--- app/bot/routers/user_routers/main_user_router.py
+++ app/bot/routers/user_routers/main_user_router.py
@@ -32,8 +32,9 @@ class Referal(StatesGroup):
 
 @main_user_router.message(F.text == MainKeyboard.get_user_kb_texts().get("check_isp"))
 async def process_check_isp(message: Message):
+    msg = await message.answer("Производится проверка...")
     await check_user_and_send_notification(message.from_user.id)
-
+    msg.delete()
 
 def is_valid_phone(phone: str) -> bool:
     return bool(re.fullmatch(r"\+?\d{10,15}", phone.strip()))
