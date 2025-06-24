50fd45d9acd1021baf83f6b7f2a0dda863e74cfc
viss <egor-berezin@bk.ru>
2025-06-24 16:21:53 +1000

    commit

--- app/bot/midlewares/check_sub.py
+++ app/bot/midlewares/check_sub.py
@@ -6,13 +6,14 @@ from aiogram.enums import ChatMemberStatus
 
 from app.bot.keyboards.inline_kb import get_subscription_on_chanel_keyboard
 from app.config import settings
+
 class CheckSub(BaseMiddleware):
     async def __call__(self, 
                        handler: Callable[[Message,Dict[str,Any]], Awaitable[Any]], 
                        event:Message, 
                        data:Dict[str,Any]) -> Any:
         chat_member = await event.bot.get_chat_member(settings.CHAT_TO_SUB, event.from_user.id)
-        
+        print(chat_member)
         if chat_member.status == 'left':
             await event.answer(
                 'Подпишись на канал, чтобы пользоваться ботом',reply_markup=get_subscription_on_chanel_keyboard()
